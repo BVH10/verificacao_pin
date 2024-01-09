e13af3f909fff53cd5f73f5424e893c32df416ff
BVH10 <155898085+BVH10@users.noreply.github.com>
2024-01-09 20:34:40 +0200

    Add files via upload

---
+++ fonte/src/pin.js
@@ -0,0 +1,43 @@
+// Importa o módulo readline para solicitar entrada do usuário
+const readline = require('readline');
+
+// Cria uma interface de leitura e escrita
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let pin;
+let numeroCorreto = 6041;
+
+// Define uma função para verificar se o PIN inserido possui quatro dígitos
+const verificarPin = () => {
+  rl.question('Digite um número (PIN) de quatro dígitos:', (input) => {
+    if (input.length !== 4) {
+      console.log('O número deve possuir quatro dígitos!');
+      verificarPin();
+    } else {
+      pin = input;
+
+      // Verifica se o número inserido é igual ao número correto
+      if (pin == numeroCorreto) {
+        console.log("Parabéns! Você acertou o PIN!");
+        rl.close();
+      } else {
+        // Verifica se o número inserido é menor, maior, muito menor ou muito maior que o número correto
+        if (pin < numeroCorreto && pin > 5940) {
+          console.log("O próximo valor deve ser maior.");
+        } else if (pin > numeroCorreto && pin < 6142) {
+          console.log("O próximo valor deve ser menor.");
+        } else if (pin > numeroCorreto && pin <= 9999) {
+          console.log("O próximo valor deve ser muito menor.");
+        } else {
+          console.log("O próximo valor deve ser muito maior.");
+        }
+        verificarPin();
+      }
+    }
+  });
+};
+
+verificarPin();
